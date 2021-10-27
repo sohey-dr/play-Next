e5ed2fe3dd0fcdbb7792c28d4c4c165280b943fb
soheyHashimoto <heyso1209@gmail.com>
2021-10-27 22:30:48 +0900

    [ADD]cssを使う

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,11 @@
-import Head from "next/head";
 import Link from "next/link";
+import Layout from "../components/layout";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="wrap">
-      <Head>
-        <title>Next.js使ってみた</title>
-        <meta name="description" content="Next.jsのテストです。" />
-      </Head>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-      <h1>Next.js使ってみた</h1>
-    </div>
+    <Layout title="ホームページタイトル" description="ホームページ概要">
+      <p className={styles.colorRed}>ホームページコンテンツ</p>
+    </Layout>
   );
 }
