1144a8e4bf9d4e364cef53eb107e2eada51179c9
soheyHashimoto <heyso1209@gmail.com>
2021-10-27 22:29:45 +0900

    [ADD]layout.tsx

---
+++ components/layout.tsx
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import Link from "next/link";
+
+type Props = {
+  children?: React.ReactNode;
+  title?: string;
+  description?: string;
+};
+
+export default function Layout({ children, title, description }: Props) {
+  const pageTitle = title || "ホームページタイトル";
+  return (
+    <div className="wrap">
+      <Head>
+        <title>{pageTitle}</title>
+        <meta name="description" content={description || "ホームページ概要"} />
+      </Head>
+      <header>
+        <h1>{pageTitle}</h1>
+      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <main>{children}</main>
+      <footer>&copy; Next.js Demo</footer>
+    </div>
+  );
+}
