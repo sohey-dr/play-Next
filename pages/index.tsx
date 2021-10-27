e3b04c4f9e313cade75c7a07aa6126c223a8ae71
soheyHashimoto <heyso1209@gmail.com>
2021-10-27 22:46:19 +0900

    [ADD]tailwindの適用

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import Layout from "../components/layout";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <Layout title="ホームページタイトル" description="ホームページ概要">
-      <p className={styles.colorRed}>ホームページコンテンツ</p>
+      <p>ホームページコンテンツ</p>
     </Layout>
   );
 }
