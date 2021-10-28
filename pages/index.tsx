6daeabb4bfc60dc04694757ffd46d73128eb8c37
soheyHashimoto <heyso1209@gmail.com>
2021-10-28 11:10:50 +0900

    [ADD]useStateとuseEffectとsetIntervalを使って1秒ごとに計算

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,17 @@
 import Layout from "../components/layout";
+import { useEffect, useState } from "react";
+
+const startTime = new Date().getTime();
 
 export default function Home() {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime((new Date().getTime() - startTime) / 1000);
+    }, 1000);
+  }, [time]);
+
   return (
     <Layout title="ホームページタイトル" description="ホームページ概要">
       <p>ホームページコンテンツ</p>
